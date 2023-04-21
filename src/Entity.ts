61a7f4c9123a7706123dceb53bd5297b51b4708f
Palladium02 <seanmeyer544@gmail.com>
2023-04-21 15:47:21 +0200

    added base implementation for Entity

--- src/Entity.ts
+++ src/Entity.ts
@@ -1,11 +1,24 @@
+import { QueryBuilder } from "./QueryBuilder";
 import type { Where, ExcludeMethods } from "./typings";
 
 export class Entity {
-  find(where: Where<ExcludeMethods<typeof this>>) {}
+  find(where: Where<ExcludeMethods<typeof this>>) {
+    return `SELECT * FROM ${
+      this.constructor.name
+    } WHERE ${new QueryBuilder().where(where)};`;
+  }
 
-  findOne(where: Where<ExcludeMethods<typeof this>>) {}
+  findOne(where: Where<ExcludeMethods<typeof this>>) {
+    return `SELECT * FROM ${
+      this.constructor.name
+    } WHERE ${new QueryBuilder().where(where)} LIMIT 1;`;
+  }
 
-  delete(where: Where<ExcludeMethods<typeof this>>) {}
+  delete(where: Where<ExcludeMethods<typeof this>>) {
+    return `DELETE FROM ${
+      this.constructor.name
+    } WHERE ${new QueryBuilder().where(where)}`;
+  }
 
   update(
     where: Where<ExcludeMethods<typeof this>>,
