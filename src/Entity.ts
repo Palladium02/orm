fc5248b2a59cc2bd0cda7b888ad4978f7c11dbd7
Palladium02 <seanmeyer544@gmail.com>
2023-04-21 16:34:53 +0200

    integrated Datasource to actually perform query

--- src/Entity.ts
+++ src/Entity.ts
@@ -1,27 +1,50 @@
 import { QueryBuilder } from "./QueryBuilder";
+import { Datasource } from "./Datasource";
 import type { Where, ExcludeMethods } from "./typings";
 
 export class Entity {
-  find(where: Where<ExcludeMethods<typeof this>>) {
-    return `SELECT * FROM ${
-      this.constructor.name
-    } WHERE ${new QueryBuilder().where(where)};`;
+  async create(data: Partial<ExcludeMethods<typeof this>>) {
+    const columns = `(${Object.keys(data).join(",")})`;
+    const values = `(${Object.values(data).join(",")})`;
+    return await Datasource.$raw(
+      `INSERT INTO ${this.constructor.name} ${columns} VALUES ${values}`
+    );
   }
 
-  findOne(where: Where<ExcludeMethods<typeof this>>) {
-    return `SELECT * FROM ${
-      this.constructor.name
-    } WHERE ${new QueryBuilder().where(where)} LIMIT 1;`;
+  async find(where: Where<ExcludeMethods<typeof this>>) {
+    return await Datasource.$raw(
+      `SELECT * FROM ${this.constructor.name} WHERE ${new QueryBuilder().where(
+        where
+      )};`
+    );
   }
 
-  delete(where: Where<ExcludeMethods<typeof this>>) {
-    return `DELETE FROM ${
-      this.constructor.name
-    } WHERE ${new QueryBuilder().where(where)}`;
+  async findOne(where: Where<ExcludeMethods<typeof this>>) {
+    return Datasource.$raw(
+      `SELECT * FROM ${this.constructor.name} WHERE ${new QueryBuilder().where(
+        where
+      )} LIMIT 1;`
+    );
   }
 
-  update(
+  async delete(where: Where<ExcludeMethods<typeof this>>) {
+    return Datasource.$raw(
+      `DELETE FROM ${this.constructor.name} WHERE ${new QueryBuilder().where(
+        where
+      )}`
+    );
+  }
+
+  async update(
     where: Where<ExcludeMethods<typeof this>>,
-    data: ExcludeMethods<typeof this>
-  ) {}
+    data: Partial<ExcludeMethods<typeof this>>
+  ) {
+    const selector = new QueryBuilder().where(where);
+    const set = Object.entries(data)
+      .map((pair) => pair.join("="))
+      .join(",");
+    return Datasource.$raw(
+      `UPDATE ${this.constructor.name} SET ${set} WHERE ${selector};`
+    );
+  }
 }
